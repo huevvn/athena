20a07021d6898edc800ce5e0d150ee0cf983fd08
Ezzeldin <ezzeldin.abdelfattah@ejust.edu.eg>
2025-04-24 22:46:33 +0200

    fix: reduce transition duration for smoother animation on "main" page

--- client/app/main/page.tsx
+++ client/app/main/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 1, ease: "easeInOut" }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
         >
             <main className="min-h-screen flex flex-col">
                 <Navbar />
